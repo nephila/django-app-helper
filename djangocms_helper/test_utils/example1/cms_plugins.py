8b85caf21bea94e4cb2774f217a4c1f01c1f2052
Iacopo Spalletti <i.spalletti@nephila.it>
2015-05-20 17:34:23 +0200

    Add a meaningful test to example1

---
+++ djangocms_helper/test_utils/example1/cms_plugins.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+
+
+class FakePlugin(CMSPluginBase):
+    name = 'FakePlugin'
+    render_template = 'fake_plugin.html'
+
+plugin_pool.register_plugin(FakePlugin)
