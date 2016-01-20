45ec313c5b1a26cf66a28d1f008bf5328ec41aa0
Iacopo Spalletti <i.spalletti@nephila.it>
2016-01-20 18:42:57 +0100

    Bump development version

--- djangocms_helper/__init__.py
+++ djangocms_helper/__init__.py
@@ -2,7 +2,7 @@
 """djangocms-helper - Helpers for django CMS plugin development"""
 from __future__ import absolute_import, print_function, unicode_literals
 
-__version__ = '0.9.4'
+__version__ = '0.9.4.post1'
 __author__ = 'Iacopo Spalletti <i.spalletti@nephila.it>'
 __all__ = ['runner']
 
