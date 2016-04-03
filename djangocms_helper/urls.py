c1c494a71713f015596c0ba5c4a2cc0625e67386
Iacopo Spalletti <i.spalletti@nephila.it>
2016-04-03 10:13:56 +0200

    Minor cleanups

--- djangocms_helper/urls.py
+++ djangocms_helper/urls.py
@@ -2,7 +2,7 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
 from django.conf import settings
-from django.conf.urls import include, patterns, url
+from django.conf.urls import include, url
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 
@@ -10,11 +10,11 @@ from .utils import load_from_file
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve',  # NOQA
         {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
     url(r'^jsi18n/(?P<packages>\S+?)/$', 'django.views.i18n.javascript_catalog'),  # NOQA
-)
+]
 urlpatterns += i18n_patterns('',
     url(r'^admin/', include(admin.site.urls)),  # NOQA
 )
