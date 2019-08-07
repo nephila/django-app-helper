a6c1dbef94b97a2aa5fdaea089e85ce528725620
Iacopo Spalletti <i.spalletti@nephila.it>
2019-08-07 11:52:03 +0200

    Fix dependencies

--- setup.py
+++ setup.py
@@ -26,8 +26,8 @@ setuptools.setup(
     install_requires=requirements,
     extras_require={
         ':python_version<"3.3"': ['mock'],
-        'pyflakes': ['pyflakes<2.1', 'django-cms<=3.4,<3.7'],
-        'cms': ['django-cms<=3.4,<3.7']
+        'pyflakes': ['pyflakes<2.1', 'django-cms>=3.4,<3.7'],
+        'cms': ['django-cms>=3.4,<3.7']
     },
     entry_points={
         'console_scripts': [
